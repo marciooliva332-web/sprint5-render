1111fdb5dcd58c935d2c7dfd95202ec40662a7c6
Márcio Oliva <marciooliva332@gmail.com>
2026-04-19 08:33:31 -0300

    Adicionando slider e botão do simulador

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
-import pandas as pd
 
-st.header('Meu Primeiro Web App no Render')
-st.write('Simulador de Moeda em breve!')
+# Título do App
+st.header('Jogando uma moeda')
+
+# Widget 1: Controle deslizante (Slider)
+# Parâmetros: Rótulo, Valor Mínimo, Valor Máximo, Valor Padrão
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+
+# Widget 2: Botão
+start_button = st.button('Executar')
+
+# Lógica que acontece quando o botão é clicado
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas...')
+    
+# Mensagem de rodapé
+st.write('Ainda não é um aplicativo funcional. Em construção.')
